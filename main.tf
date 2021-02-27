69e1ae61df8a548b0eb66398690b90c5b79a9e6a
Oleksii Shkurupii <Oleksii.Shkurupii@playtech.com>
2021-02-27 18:27:45 +0200

    Added assume role and switched workspace to new iam user

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ terraform {
 }
 
 provider "aws" {
+  alias = "develop"
+  assume_role {
+    role_arn = var.assume_role_arn
+  }
+}
+
+variable "assume_role_arn" {
+  type = string
+  description = "Assume role ARN"
 }
 
 variable "bucket_name" {
@@ -12,6 +21,35 @@ variable "bucket_name" {
   description = "Bucket Name"
 }
 
+variable "workspace" {
+  type = string
+  description = "Terraform workspace"
+}
+
+variable "environment" {
+  type = string
+  description = "Environment name"
+}
+
+variable "username" {
+  type = string
+  description = "User name"
+}
+
+variable "domain_name" {
+  type = string
+  description = "Domain Name"
+}
+
 resource "aws_s3_bucket" "test-bucket" {
+  provider = aws.develop
+  force_destroy = true
   bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket" "dev-bucket" {
+  provider = aws.develop
+  force_destroy = true
+  bucket_prefix = "${var.workspace}-bucket-"
+  tags = module.tags.all_tags
 }
