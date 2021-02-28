02d991b50b32bd96a0689f7d22d9fe27c30c3ff2
Oleksii Shkurupii <Oleksii.Shkurupii@playtech.com>
2021-02-28 18:07:13 +0200

    Removed custom VPC

---
+++ vpc-default.tf
@@ -0,0 +1,14 @@
+resource "aws_default_vpc" "default" {}
+
+data "aws_subnet_ids" "default_subtets" {
+  vpc_id = aws_default_vpc.default.id
+}
+
+data "aws_security_group" "default" {
+  vpc_id = aws_default_vpc.default.id
+  name = "default"
+}
+
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
